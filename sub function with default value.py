a1e801dda053bb1f545c43f0992465e8669c026a
shadrack hamesh . S <166796546+shadrackhamesh02@users.noreply.github.com>
2024-05-12 20:55:32 +0530

    Add files via upload

---
+++ sub function with default value.py
@@ -0,0 +1,12 @@
+# function without arguments and without return type (default value)
+# sub function
+
+def sub():
+    a = 1000
+    b = 500
+    c = a-b
+    print(c)
+
+    # call function 
+
+sub()
